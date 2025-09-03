3e5226f3f622705f2c32de2e3bd9516c992dee5e
Dominic_SE <105536723+SE-Dominic@users.noreply.github.com>
2025-09-03 17:52:47 +0000

    insertion sort

---
+++ Naive Sorts/naive.cpp
@@ -0,0 +1,38 @@
+//insertion sort, selection sort, bubble sort
+#include <iostream>
+using namespace std;
+
+template <typename T>
+void _swap(T& a, T& b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
+template <typename T>
+void insertionSort(T ar[], int length) {
+    //loop through everything after first element
+    //key is where we know to check the left side
+    //need iterator to check left side
+    //shift elements
+    for (int i = 1; i < length; i++) {
+        T key = ar[i];
+        int j = i - 1;
+        while (j >= 0 && ar[j] > key) {
+            ar[j + 1] = ar[j];
+            j = j - 1;
+        }
+        ar[j + 1] = key;
+    }
+
+}
+
+int main() {
+    int a[] = {5, 9, 7, 8, 12, 4, 6};
+    cout << "Hello world" << endl;
+    insertionSort(a, 7);
+    for (int i = 0; i < 7; i++) {
+        cout << a[i] << " ";
+    }
+    cout << "\n\n\n";
+    return 0;
+}
